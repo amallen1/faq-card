1b29e92dee078a62564a8ef4878f77261b8f92ae
Aniya <amallen046@gmail.com>
2021-07-29 20:45:30 -0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const questions = Array.from(document.getElementsByClassName('question'));
+const answers = Array.from(document.getElementsByClassName('answer'));
 
 console.log(questions);
 
 questions.forEach(question => {
-    question.addEventListener('click', (e) => {
+    question.addEventListener('click', () => {
         question.classList.toggle('clicked');
     })
 })
